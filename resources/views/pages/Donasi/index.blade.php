636af57624935109cb0eb3a4bbede919cd63b7a6
Ade Ripaldi <118891086+revaile@users.noreply.github.com>
2024-06-24 18:02:30 +0700

    Update index.blade.php
    final

--- resources/views/pages/Donasi/index.blade.php
+++ resources/views/pages/Donasi/index.blade.php
@@ -1,7 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
 <link href="css/styles.css" rel="stylesheet" />
 @extends('layouts.main')
-
+<!-- final -->
 @section('content')
     <div class="card mb-4">
         <div class="card-header">
